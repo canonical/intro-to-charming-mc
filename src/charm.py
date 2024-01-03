a87e35465a36b495c08dcae5a5b5890b0dd1d2e4
Pietro Pasotti <starfire.daemon@gmail.com>
2024-01-03 14:47:06 +0100

    basic installation

--- src/charm.py
+++ src/charm.py
@@ -13,10 +13,12 @@ https://juju.is/docs/sdk/create-a-minimal-kubernetes-charm
 """
 
 import logging
+import shlex
+from subprocess import Popen
 
 import ops
+from ops import MaintenanceStatus, ActiveStatus
 
-# Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
 
 VALID_LOG_LEVELS = ["info", "debug", "warning", "error", "critical"]
@@ -27,77 +29,49 @@ class IntroToCharmingMcCharm(ops.CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on['httpbin'].pebble_ready, self._on_httpbin_pebble_ready)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.install, self._on_install)
 
-    def _on_httpbin_pebble_ready(self, event: ops.PebbleReadyEvent):
-        """Define and start a workload using the Pebble API.
+    def _on_install(self, event):
+        self._install_nginx()  # this will also start it by default
+        self._enable_nginx_firewall_profile()
+        self.status = ActiveStatus()
 
-        Change this example to suit your needs. You'll need to specify the right entrypoint and
-        environment configuration for your specific workload.
+    def _install_nginx(self):
+        self.status = MaintenanceStatus("installing nginx...")
 
-        Learn more about interacting with Pebble at at https://juju.is/docs/sdk/pebble.
-        """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Add initial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", self._pebble_layer, combine=True)
-        # Make Pebble reevaluate its plan, ensuring any services are started if enabled.
-        container.replan()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
-        self.unit.status = ops.ActiveStatus()
+        script = (
+            "apt update",
+            "apt install nginx",
+        )
+        for line in script:
+            proc = Popen(shlex.split(line))
+            proc.wait()
 
-    def _on_config_changed(self, event: ops.ConfigChangedEvent):
-        """Handle changed configuration.
+    def _enable_nginx_firewall_profile(self):
+        self.status = MaintenanceStatus("setting up ufw rules...")
 
-        Change this example to suit your needs. If you don't need to handle config, you can remove
-        this method.
+        proc = Popen(shlex.split("ufw allow 'Nginx HTTP'"))
+        proc.wait()
 
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        # Fetch the new config value
-        log_level = self.model.config["log-level"].lower()
+    def _start_nginx(self, restart: bool = False):
+        self.status = MaintenanceStatus("starting nginx...")
 
-        # Do some validation of the configuration option
-        if log_level in VALID_LOG_LEVELS:
-            # The config is good, so update the configuration of the workload
-            container = self.unit.get_container("httpbin")
-            # Verify that we can connect to the Pebble API in the workload container
-            if container.can_connect():
-                # Push an updated layer with the new config
-                container.add_layer("httpbin", self._pebble_layer, combine=True)
-                container.replan()
+        verb = "restart" if restart else "start"
+        proc = Popen(shlex.split(f"systemctl {verb} nginx"))
+        proc.wait()
 
-                logger.debug("Log level for gunicorn changed to '%s'", log_level)
-                self.unit.status = ops.ActiveStatus()
-            else:
-                # We were unable to connect to the Pebble API, so we defer this event
-                event.defer()
-                self.unit.status = ops.WaitingStatus("waiting for Pebble API")
-        else:
-            # In this case, the config option is bad, so block the charm and notify the operator.
-            self.unit.status = ops.BlockedStatus("invalid log level: '{log_level}'")
+    def _reload_nginx(self):
+        self.status = MaintenanceStatus("reloading nginx...")
 
-    @property
-    def _pebble_layer(self) -> ops.pebble.LayerDict:
-        """Return a dictionary representing a Pebble layer."""
-        return {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
-            "services": {
-                "httpbin": {
-                    "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
-                    "startup": "enabled",
-                    "environment": {
-                        "GUNICORN_CMD_ARGS": f"--log-level {self.model.config['log-level']}"
-                    },
-                }
-            },
-        }
+        proc = Popen(shlex.split(f"systemctl reload nginx"))
+        proc.wait()
+
+    def _stop_nginx(self):
+        self.status = MaintenanceStatus("stopping nginx...")
+
+        proc = Popen(shlex.split(f"systemctl stop nginx"))
+        proc.wait()
 
 
-if __name__ == "__main__":  # pragma: nocover
-    ops.main(IntroToCharmingMcCharm)  # type: ignore
+if __name__ == "__main__":
+    ops.main.main(IntroToCharmingMcCharm)
