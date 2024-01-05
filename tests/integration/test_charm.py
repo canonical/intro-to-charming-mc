e9b6ae3bb545e61611bd18c8551d49de23d144a0
Pietro Pasotti <starfire.daemon@gmail.com>
2024-01-05 15:15:02 +0100

    tester charm

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -4,6 +4,8 @@
 
 import asyncio
 import logging
+import os
+import shutil
 from pathlib import Path
 
 import pytest
@@ -12,7 +14,7 @@ from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 APP_NAME = METADATA["name"]
 
 
@@ -24,12 +26,20 @@ async def test_build_and_deploy(ops_test: OpsTest):
     """
     # Build and deploy charm from local source folder
     charm = await ops_test.build_charm(".")
-    resources = {"httpbin-image": METADATA["resources"]["httpbin-image"]["upstream-source"]}
 
     # Deploy the charm and wait for active/idle status
     await asyncio.gather(
-        ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME),
+        ops_test.model.deploy(charm, application_name=APP_NAME),
         ops_test.model.wait_for_idle(
             apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
         ),
     )
+
+
+@pytest.fixture(scope="module", autouse=True)
+def copy_lib_into_tester_charm(ops_test):
+    # ensure tester copy is up-to-date if we make changes to the base lib
+    lib = "intro_to_charming_mc/v0/demo_ingress.py"
+    install_path = f"tests/integration/tester/lib/charms/{lib}"
+    os.makedirs(os.path.dirname(install_path), exist_ok=True)
+    shutil.copyfile(f"lib/charms/{lib}", install_path)
---
+++ tests/integration/tester/lib/charms/intro_to_charming_mc/v0/demo_ingress.py
@@ -0,0 +1,81 @@
+"""Demo library to offer demo ingress integrations for intro-to-charming-mc nginx charm.
+"""
+import logging
+import socket
+from typing import List, Tuple, Optional
+
+import ops
+
+logger = logging.getLogger(__name__)
+
+# The unique Charmhub library identifier, never change it
+LIBID = "4e46e1e7f79f4164a9d21dd79e354ff0"
+
+# Increment this major API version when introducing breaking changes
+LIBAPI = 0
+
+# Increment this PATCH version before using `charmcraft publish-lib` or reset
+# to 0 if you are raising the major API version
+LIBPATCH = 1
+
+DEFAULT_ENDPOINT = "demo-ingress"
+
+
+class IngressProvider:
+    def __init__(self, charm: ops.CharmBase, endpoint_name: str = DEFAULT_ENDPOINT):
+        self._charm = charm
+        self._endpoint_name = endpoint_name
+
+    @property
+    def relations(self) -> List[ops.Relation]:
+        return self._charm.model.relations[self._endpoint_name]
+
+    def requests(self) -> List[Tuple[ops.Relation, Tuple[str, int]]]:
+        reqs = []
+
+        for relation in self.relations:
+            if not relation.app or not relation.data:
+                logger.debug(f"relation {relation} not ready: no app")
+                continue
+
+            app_data = relation.data[relation.app]
+
+            if not app_data:
+                logger.debug(f"relation {relation} not ready: remote did not request() yet")
+                continue
+
+            try:
+                port = int(app_data['port'])
+                host = app_data['host']
+            except (TypeError, KeyError):
+                logger.error(f"failed validating databag contents for {relation}", exc_info=True)
+                continue
+
+            reqs.append((relation, (host, port)))
+
+        return reqs
+
+    def respond(self, relation: ops.Relation, url: str):
+        relation.data[self._charm.app]['url'] = url
+
+
+class IngressRequirer:
+    def __init__(self, charm: ops.CharmBase, endpoint_name: str = DEFAULT_ENDPOINT):
+        self._charm = charm
+        self._endpoint_name = endpoint_name
+
+    @property
+    def relation(self) -> ops.Relation:
+        return self._charm.model.get_relation(self._endpoint_name)
+
+    def request(self, port: int, host: str = None):
+        host = host or socket.getfqdn()
+        if relation := self.relation:
+            relation.data[self._charm.app]['port'] = str(port)
+            relation.data[self._charm.app]['host'] = str(host)
+
+    @property
+    def response(self) -> Optional[str]:
+        if relation := self.relation:
+            return relation.data[relation.app].get('url')
+        return None
